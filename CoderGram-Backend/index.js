60d42bb9efcdce1146e6b295798ce0f1fbd4a5ee
DevaanshKathuria <devaanshkathuria@gmail.com>
2025-11-30 13:25:26 +0530

    Added cors to index.js and required path module

--- CoderGram-Backend/index.js
+++ CoderGram-Backend/index.js
@@ -11,6 +11,10 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
+const path = require('path');
+const cors = require('cors');
+app.use(cors());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI)
