a505ee981503491b0713524e96663d091e71c406
DevaanshKathuria <devaanshkathuria@gmail.com>
2025-11-30 13:45:51 +0530

    Made final fixes to the app

--- CoderGram-FrontEnd/config.js
+++ CoderGram-FrontEnd/config.js
@@ -1,10 +1,10 @@
-// API Configuration
-export const API_URL = 'http://localhost:8000/api';
+export const API_BASE = 'http://localhost:8000/api';
 
-// Add other configuration constants here
+export const API_URL = API_BASE;
 export const APP_NAME = 'CoderGram';
 
 export default {
+  API_BASE,
   API_URL,
   APP_NAME,
 };
